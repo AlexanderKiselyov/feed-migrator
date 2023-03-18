db97347056c0f5d035a49da1a09ded2cbc4e094c
ayasevich <artem.yasevich@mail.ru>
2023-03-18 21:26:43 +0300

    Cleanup ok-api's build.gradle

--- ok-api/build.gradle.kts
+++ ok-api/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-    id("java")
-    id("checkstyle")
+    java
+    checkstyle
 }
 
 group = "polis"
@@ -11,15 +11,12 @@ repositories {
 }
 
 dependencies {
-    implementation("com.puppycrawl.tools:checkstyle:10.3.3")
-    implementation("commons-codec:commons-codec:1.15")
     implementation("org.apache.httpcomponents:httpclient:4.5.14")
+    implementation("org.apache.httpcomponents:httpmime:4.5.14")
+    implementation("commons-codec:commons-codec:1.15")
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.14.2")
     implementation("org.json:json:20230227")
-    implementation("org.telegram:telegrambots:6.5.0")
-    implementation("org.telegram:telegrambotsextensions:6.5.0")
-    implementation("org.projectlombok:lombok:1.18.26")
-    implementation("org.springframework.boot:spring-boot-starter-web:3.0.4")
-    implementation("org.telegram:telegrambots-spring-boot-starter:6.5.0")
+    implementation("com.puppycrawl.tools:checkstyle:10.3.3")
 
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
