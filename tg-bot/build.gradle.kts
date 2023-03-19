329914fc45422ca51e3b497839d92bc1d5bd00a8
ayasevich <artem.yasevich@mail.ru>
2023-03-19 14:40:04 +0300

    tg-bot: add explicit dependencies for logging

--- tg-bot/build.gradle.kts
+++ tg-bot/build.gradle.kts
@@ -18,6 +18,9 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web:3.0.4")
     implementation("org.telegram:telegrambots-spring-boot-starter:6.5.0")
 
+    implementation("org.slf4j:slf4j-api:2.0.7")
+    runtimeOnly("org.slf4j:slf4j-simple:2.0.7")
+
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
 }
