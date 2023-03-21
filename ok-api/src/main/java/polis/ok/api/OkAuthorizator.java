0ef86e0d6ec97ab1b057bbf3f0be9e4c6ba661a0
Artem Yasevich <70908206+a-yasevich@users.noreply.github.com>
2023-03-21 21:38:55 +0300

    Okapi implementation (#18)
    okapi: exception handling

--- ok-api/src/main/java/polis/ok/api/OkAuthorizator.java
+++ ok-api/src/main/java/polis/ok/api/OkAuthorizator.java
@@ -2,22 +2,31 @@ package polis.ok.api;
 
 import org.apache.commons.codec.digest.DigestUtils;
 import org.apache.http.client.utils.URIBuilder;
+import org.json.JSONException;
 import org.json.JSONObject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 
+import static polis.ok.api.LoggingUtils.parseResponse;
+import static polis.ok.api.LoggingUtils.sendRequest;
+import static polis.ok.api.LoggingUtils.wrapAndLog;
+
 public final class OkAuthorizator {
     private static final String AUTH_URI = "https://connect.ok.ru/oauth/authorize";
     private static final String GET_TOKEN_URI = "https://api.ok.ru/oauth/token.do";
     private static final String APP_SCOPE = "VALUABLE_ACCESS;LONG_ACCESS_TOKEN;PHOTO_CONTENT;GROUP_CONTENT;VIDEO_CONTENT";
 
+    private static final Logger logger = LoggerFactory.getLogger(OkAuthorizator.class);
     private final HttpClient client = HttpClient.newHttpClient();
 
-    public TokenPair getToken(String code) throws Exception {
+    public TokenPair getToken(String code) throws IOException, URISyntaxException, OkApiException {
         URI uri = new URIBuilder(GET_TOKEN_URI)
                 .addParameter("code", code)
                 .addParameter("client_id", OkAppProperties.APPLICATION_ID)
@@ -25,19 +34,20 @@ public final class OkAuthorizator {
                 .addParameter("redirect_uri", OkAppProperties.REDIRECT_URI)
                 .addParameter("grant_type", "authorization_code")
                 .build();
-
         HttpRequest request = HttpRequest.newBuilder().POST(HttpRequest.BodyPublishers.ofByteArray(new byte[]{}))
                 .uri(uri)
                 .build();
-        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
-        JSONObject responseJson = new JSONObject(response.body());
+        HttpResponse<String> response = sendRequest(client, request, logger);
+        JSONObject responseJson = parseResponse(response, logger);
 
-        if (responseJson.has("error")) {
-            return new TokenPair(null, null);
+        try {
+            return new TokenPair(
+                    responseJson.getString("access_token"),
+                    responseJson.getString("refresh_token")
+            );
+        } catch (JSONException e) {
+            throw wrapAndLog(e, response.toString(), response.body(), logger);
         }
-        String accessToken = responseJson.getString("access_token");
-        String refreshToken = responseJson.getString("refresh_token");
-        return new TokenPair(accessToken, refreshToken);
     }
 
     public static String formAuthorizationUrl() throws URISyntaxException {
@@ -47,7 +57,6 @@ public final class OkAuthorizator {
                 .addParameter("response_type", "code")
                 .addParameter("redirect_uri", OkAppProperties.REDIRECT_URI)
                 .build();
-
         return uri.toString();
     }
 
@@ -64,4 +73,5 @@ public final class OkAuthorizator {
 
     public record TokenPair(String accessToken, String refreshToken) {
     }
+
 }
