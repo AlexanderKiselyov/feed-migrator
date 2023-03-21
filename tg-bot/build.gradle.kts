8ddddcc66765c17440169227d293acbff280010c
AlexanderKiselyov <a_kiselyov-snz@mail.ru>
2023-03-21 16:54:40 +0300

    Project dependencies fix

--- tg-bot/build.gradle.kts
+++ tg-bot/build.gradle.kts
@@ -19,10 +19,9 @@ dependencies {
     implementation("org.telegram:telegrambots-spring-boot-starter:6.5.0")
 
     implementation("org.slf4j:slf4j-api:2.0.7")
-    runtimeOnly("org.slf4j:slf4j-simple:2.0.7")
 
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.9.1")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.9.1")
 }
 
 tasks.getByName<Test>("test") {
