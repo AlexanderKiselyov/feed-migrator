c6d3f63c9cf9f19c48da6d1a6fc5a3ec32a70692
ayasevich <artem.yasevich@mail.ru>
2023-04-16 23:37:38 +0300

    ok-api: refreshToken() method

--- ok-api/src/main/java/polis/ok/api/OkAuthorizator.java
+++ ok-api/src/main/java/polis/ok/api/OkAuthorizator.java
@@ -19,16 +19,15 @@ import static polis.ok.api.LoggingUtils.sendRequest;
 import static polis.ok.api.LoggingUtils.wrapAndLog;
 
 public final class OkAuthorizator {
-    private static final String AUTH_URI = "https://connect.ok.ru/oauth/authorize";
-    private static final String GET_TOKEN_URI = "https://api.ok.ru/oauth/token.do";
-    private static final String APP_SCOPE =
-            "VALUABLE_ACCESS;LONG_ACCESS_TOKEN;PHOTO_CONTENT;GROUP_CONTENT;VIDEO_CONTENT";
+    private static final String AUTH_URL = "https://connect.ok.ru/oauth/authorize";
+    private static final String TOKEN_URL = "https://api.ok.ru/oauth/token.do";
+    private static final String APP_SCOPE = "VALUABLE_ACCESS;LONG_ACCESS_TOKEN;PHOTO_CONTENT;GROUP_CONTENT;VIDEO_CONTENT";
 
     private static final Logger logger = LoggerFactory.getLogger(OkAuthorizator.class);
     private final HttpClient client = HttpClient.newHttpClient();
 
     public TokenPair getToken(String code) throws IOException, URISyntaxException, OkApiException {
-        URI uri = new URIBuilder(GET_TOKEN_URI)
+        URI uri = new URIBuilder(TOKEN_URL)
                 .addParameter("code", code)
                 .addParameter("client_id", OkAppProperties.APPLICATION_ID)
                 .addParameter("client_secret", OkAppProperties.APPLICATION_SECRET_KEY)
@@ -51,8 +50,32 @@ public final class OkAuthorizator {
         }
     }
 
+    public TokenPair refreshToken(String refreshToken) throws URISyntaxException, IOException, OkApiException {
+        URI uri = new URIBuilder(TOKEN_URL)
+                .addParameter("refresh_token", refreshToken)
+                .addParameter("client_id", OkAppProperties.APPLICATION_ID)
+                .addParameter("client_secret", OkAppProperties.APPLICATION_SECRET_KEY)
+                .addParameter("redirect_uri", OkAppProperties.REDIRECT_URI)
+                .addParameter("grant_type", " refresh_token")
+                .build();
+        HttpRequest request = HttpRequest.newBuilder().POST(HttpRequest.BodyPublishers.ofByteArray(new byte[]{}))
+                .uri(uri)
+                .build();
+        HttpResponse<String> response = sendRequest(client, request, logger);
+        JSONObject responseJson = parseResponse(response, logger);
+
+        try {
+            return new TokenPair(
+                    responseJson.getString("access_token"),
+                    refreshToken
+            );
+        } catch (JSONException e) {
+            throw wrapAndLog(e, response.toString(), response.body(), logger);
+        }
+    }
+
     public static String formAuthorizationUrl() throws URISyntaxException {
-        URI uri = new URIBuilder(AUTH_URI)
+        URI uri = new URIBuilder(AUTH_URL)
                 .addParameter("client_id", OkAppProperties.APPLICATION_ID)
                 .addParameter("scope", APP_SCOPE)
                 .addParameter("response_type", "code")
