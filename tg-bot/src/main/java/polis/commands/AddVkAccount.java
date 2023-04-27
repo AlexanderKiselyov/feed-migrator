919e518624f8a4e3eb2f7d8b40dcea0dd9d24c30
AlexanderKiselyov <a_kiselyov-snz@mail.ru>
2023-04-28 01:21:31 +0300

    codestyle fixes

--- tg-bot/src/main/java/polis/commands/AddVkAccount.java
+++ tg-bot/src/main/java/polis/commands/AddVkAccount.java
@@ -5,8 +5,8 @@ import org.slf4j.LoggerFactory;
 import org.telegram.telegrambots.meta.api.objects.Chat;
 import org.telegram.telegrambots.meta.api.objects.User;
 import org.telegram.telegrambots.meta.bots.AbsSender;
-import polis.vk.api.VkAuthorizator;
 import polis.util.State;
+import polis.vk.api.VkAuthorizator;
 
 import java.net.URISyntaxException;
 
