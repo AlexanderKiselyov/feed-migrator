e2a330c05fc79f86896c52f4efce02a8b5edada6
ayasevich <artem.yasevich@mail.ru>
2023-04-30 12:43:14 +0300

    Redundant imports

--- tg-bot/src/main/java/polis/bot/TgFileLoader.java
+++ tg-bot/src/main/java/polis/bot/TgFileLoader.java
@@ -5,8 +5,6 @@ import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import java.io.File;
 import java.io.IOException;
 import java.net.URISyntaxException;
-import java.nio.file.Files;
-import java.nio.file.Path;
 
 public interface TgFileLoader {
     File downloadFileById(String fileId) throws URISyntaxException, IOException, TelegramApiException;
