8c412795503662770695bc8b566546e9f637a4db
ayasevich <artem.yasevich@mail.ru>
2023-07-09 01:19:31 +0300

    Use YesNoCallback

--- tg-bot/src/main/java/polis/keyboards/callbacks/objects/YesNoCallback.java
+++ tg-bot/src/main/java/polis/keyboards/callbacks/objects/YesNoCallback.java
@@ -1,9 +1,12 @@
 package polis.keyboards.callbacks.objects;
 
 public final class YesNoCallback extends Callback {
+    public static final YesNoCallback YES_CALLBACK = new YesNoCallback(true);
+    public static final YesNoCallback NO_CALLBACK = new YesNoCallback(false);
+
     public final boolean yesOrNo;
 
-    public YesNoCallback(boolean yesOrNo) {
+    private YesNoCallback(boolean yesOrNo) {
         this.yesOrNo = yesOrNo;
     }
 }
