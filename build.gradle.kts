16e640eefc9c7791d1b32908a7aba2737fe9e21c
ayasevich <artem.yasevich@mail.ru>
2023-09-24 16:24:44 +0300

    Remove SNAPSHOT in version properties

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,23 +4,14 @@ plugins {
 }
 
 group = "polis"
-version = "1.0-SNAPSHOT"
+version = "1.0.0"
 
 repositories {
     mavenCentral()
 }
 
-dependencies {
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
-}
-
 tasks.withType<JavaCompile> {
     options.encoding = "UTF-8"
-}
-
-tasks.getByName<Test>("test") {
-    useJUnitPlatform()
 }
 
 subprojects {
